41e3dcadd6801061e3e2bfe23e32c20518178926
1870673175@qq.com <1870673175@qq.com>
2022-12-07 11:19:10 +0800

    just test！！！

--- 2.py
+++ 2.py
@@ -17,7 +17,7 @@ subprocess.call(cmd, shell=True)
 cmd = 'git remote remove origin'
 subprocess.call(cmd, shell=True)
 
-cmd = "git remote add origin https://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@github.com/huojiayang/test123.git"
+cmd = "git remote add origin https://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@github.com/huojiayang/test123.git"
 subprocess.call(cmd, shell=True)
 
 cmd = "git push -u origin master"
